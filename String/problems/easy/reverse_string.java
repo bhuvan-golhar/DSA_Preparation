01b0ac09ecaea7aed0a39df44748c7c305e08caa
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-05-08 19:49:24 +0530

    feat: add reverse of string

---
+++ String/problems/easy/reverse_string.java
@@ -0,0 +1,16 @@
+// Problem: Reverse a String
+// Approach: Reverse Traversal
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public String reverseString(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return reversed;
+    }
+}
