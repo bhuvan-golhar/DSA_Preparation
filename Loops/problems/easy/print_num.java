21dbc833902558c33b625037121ab3d362ae2379
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-05-08 19:52:58 +0530

    feat: add print numbers from 1 to n

---
+++ Loops/problems/easy/print_num.java
@@ -0,0 +1,13 @@
+// Problem: Print Numbers from 1 to N
+// Approach: Basic Iteration
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public void printNumbers(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
