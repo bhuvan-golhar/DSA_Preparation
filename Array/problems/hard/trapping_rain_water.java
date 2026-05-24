5abc3a81fdb8fc2936c1c9a85f22f449629aa9a5
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-05-24 22:38:34 +0530

    feat: add trapping rain water

---
+++ Array/problems/hard/trapping_rain_water.java
@@ -0,0 +1,42 @@
+// Problem: Trapping Rain Water
+// Approach: Two Pointer Optimization
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int trapRainWater(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+        int trappedWater = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    trappedWater += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return trappedWater;
+    }
+}
