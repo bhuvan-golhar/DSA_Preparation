0432aba8dda2762a879c82b860432e9c81629a90
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-05-24 22:24:01 +0530

    feat: add container with most water

---
+++ Array/problems/hard/container_with_most_water.java
@@ -0,0 +1,30 @@
+// Problem: Container With Most Water
+// Approach: Two Pointer Optimization
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int maxWater(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int area = width * currentHeight;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
