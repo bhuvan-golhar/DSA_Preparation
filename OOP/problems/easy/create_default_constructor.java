fc16c986c7f289b2b1f267c34ac2e981287c90f2
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-14 23:10:17 +0530

    feat: add default constructor usage

---
+++ OOP/problems/easy/create_default_constructor.java
@@ -0,0 +1,19 @@
+// Problem: Create and Use a Default Constructor
+// Concept: Default Constructor
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Student {
+
+    Student() {
+        System.out.println("Student Object Created");
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+    }
+}
